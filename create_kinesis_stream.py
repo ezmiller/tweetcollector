ea3292aa7716e3bfd106856c062d1635b543af2a
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-04-11 20:14:51 -0400

    Clean up

--- create_kinesis_stream.py
+++
@@ -1,9 +0,0 @@
-# Simple script to create a kinesis stream
-# create-stream.py
-import boto3
-
-client = boto3.client('kinesis')
-response = client.create_stream(
-   StreamName='twitter',
-   ShardCount=1
-)
